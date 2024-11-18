2af9da8172a9441abc2f4576be3eaa95496406d0
Mary Kristine De Jose <124106658+wyzywyg@users.noreply.github.com>
2024-11-18 16:12:17 +0800

    Update script.js

--- Laboratory Activities /Laboratory Activity 3-18-11-2024/script.js
+++ Laboratory Activities /Laboratory Activity 3-18-11-2024/script.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
     const textInput = document.getElementById('textInput');
     const wordCount = document.getElementById('wordCount');
